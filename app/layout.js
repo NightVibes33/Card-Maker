991cafb951acc4e2771f54e2c3ea3b804cdec9c0
Zyn <bobbytatum999@gmail.com>
2026-09-24 21:50:24 -0500

    Add Vercel Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   title: {
@@ -36,6 +37,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
